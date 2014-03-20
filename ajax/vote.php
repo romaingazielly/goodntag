5c491ea843deeb89bc43c60e4798ba9baa6ef619
Leo Hetsch <leo.hetsch@gmail.com>
2014-03-20 22:27:11 +0100

    Votes and favs

--- ajax/vote.php
+++ ajax/vote.php
@@ -6,15 +6,36 @@ require 'init.php';
 if (isset($_POST['id']) && isset($_POST['vote'])) {
 	$id = (int) $_POST['id'];
 	$vote = $_POST['vote'];
+
+	// Update de l'user pour ce vote
+
+	$userId = get_current_user_id();
+
+	$voteUser = get_user_meta($userId, 'product_votes', true);
+
+	if (null == $voteUser) {
+		$voteUser = $id;
+	} else {
+		$voteUser .= ','.$id;
+	}
+
+	update_user_meta($userId, 'product_votes', $voteUser);
+	update_user_meta($userId, 'product_vote_'.$id, $vote);
+
+
+	// Update du produit
+	
 	$voteCounter = (int) get_post_meta($id, 'votes_'.$vote, true);
 
 	if (!$voteCounter) {
 		$voteCounter = 0;
 	}
-	
+
 	$voteCounter++;
 	update_post_meta($id, 'votes_'.$vote, $voteCounter);
 
+
+	
 	echo json_encode(array(
 		'code' => 0
 	));
